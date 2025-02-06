cc752da1932e7056e78e94c195e4620242752a52
Mario-Gozie <oguedoihucg@gmail.com>
2025-02-06 13:38:52 +0200

    Initial commit

---
+++ app.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    printf("hello world\n");
+    return 0;
+}
+
+// Data types
+// int = integer
+// float = floating point number (decimals). this is typically 4bytes(32bits), so it has precision of about 7 decimal digits. used for storing smaller size of numbers.
+// double = This is for decimal but 4bytes (64bits). it has a precision of about 15 decimal digits. so it is good for large size numbers
+// Char = A single character
+// Array = collection of elements of the same data type.
+// pointer = stores memory address of another variabe.
+// structure = allows grouping variable of different data type under a single name.
+// union = similar to structure but shares the same memory location for all its members.
+// enum = Defines a set of named integer constant
+
+
+// In C, you must declare the data type of a variable before you use it
+int age;
+float salary;
+char grade;
